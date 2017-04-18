a1ca55bf02dca2911d64e1e7219df34a5eefe1bb
Daniel Somarribas <sq.daniel@gmail.com>
2017-04-18 14:04:01 -0600

    arreglos E S T E T I C O S 2.0

--- gulpfile.js
+++ gulpfile.js
@@ -9,17 +9,23 @@ var browserSync = require( 'browser-sync' )
 // Compile Our Sass
 gulp.task( 'sass', function () {
 	return gulp.src( 'app/scss/**/*.scss' ) // Gets all files ending with .scss in app/scss
-		.pipe( sass() )
+		.pipe( sass()
+			.on( 'error', function ( err ) {
+				console.error( err.message );
+				browserSync.notify( err.message, 3000 ); // Display error in the browser
+				this.emit( 'end' ); // Prevent gulp from catching the error and exiting the watch process
+			} ) )
 		.pipe( gulp.dest( 'app/css' ) )
-		.pipe( browserSync.reload( {
-			stream: true
-		} ) )
+
+		.pipe( browserSync.stream() );
 } );
 
 // Watch Files For Changes
 gulp.task( 'watch', [ 'browserSync', 'sass' ], function () {
 	gulp.watch( 'app/scss/**/*.scss', [ 'sass' ] );
 	// Other watchers
+	gulp.watch( "app/*.html" )
+		.on( 'change', browserSync.reload );
 } );
 
 // BrowserSync
